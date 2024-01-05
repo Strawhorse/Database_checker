7382d8976340de3cbdccf17e1d45094675af2182
Sean B <strawhorse@gmail.com>
2024-01-05 16:56:04 +0000

    Removed unnecessary code

--- Database_checker/src/Database_checker.java
+++ Database_checker/src/Database_checker.java
@@ -7,23 +7,47 @@ public class Database_checker extends JFrame {
 
     JMenuBar menuBar;
     JMenu menu;
-//    JMenu othermenus;
+    JMenu menu2;
+    JMenuItem m1;
+    JMenuItem m2;
+    JMenuItem m3;
+    JMenuItem m4;
+    JMenuItem m5;
 
     Database_checker() {
 
+//        set out the JFrame first
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800,800);
         this.setTitle("Database checker");
         this.setLocationRelativeTo(null);
         this.setLayout(new FlowLayout());
 
+//        Then the menu bar and the menu/s
         menuBar = new JMenuBar();
-        menu = new JMenu("File");
+        menu = new JMenu("Databases");
+        menu2 = new JMenu("Other");
 
-//        Now add menu to menu bar
-        
+//        Now add menus to menu bar
+        menuBar.add(menu);
+        menuBar.add(menu2);
+
+//        add items to the menus
+        m1 = new JMenuItem("List Databases");
+        m2 = new JMenuItem("List Database Entries");
+        m3 = new JMenuItem("Delete Database/s");
+        m4 = new JMenuItem("Create New Database/s");
+        m5 = new JMenuItem("Close Program");
+
+        menu.add(m1);
+        menu.add(m2);
+        menu.add(m3);
+        menu.add(m4);
+
+        menu2.add(m5);
 
 
+//        finally, set the menu bar to the frame
         this.setJMenuBar(menuBar);
 
 
@@ -31,28 +55,8 @@ public class Database_checker extends JFrame {
         this.setVisible(true);
 
 
-
-
-//        mb = new JMenuBar();
-//        x = new JMenu();
-//
-//        m1 = new JMenuItem("List Databases");
-//        m2 = new JMenuItem("List Database Entries");
-//        m3 = new JMenuItem("Delete Database/s");
-//        m4 = new JMenuItem("Create New Database/s");
-//        m5 = new JMenuItem("Close Program");
-//
-//        x.add(m1);
-//        x.add(m2);
-//        x.add(m3);
-//        x.add(m4);
-//        x.add(m5);
-//        mb.add(x);
-//
-//        f.add(mb, BorderLayout.NORTH);
-//        f.setJMenuBar(mb);
-
     }
+
 
 
 
