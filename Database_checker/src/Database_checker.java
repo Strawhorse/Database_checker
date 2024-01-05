0be8315295dc6f68b4de562eba2b36287fe36b0d
Sean B <strawhorse@gmail.com>
2024-01-05 16:33:07 +0000

    Restructuring layout more clearly

--- Database_checker/src/Database_checker.java
+++ Database_checker/src/Database_checker.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
 import java.sql.SQLException;
-import java.util.Scanner;
+
 
 public class Database_checker extends JFrame {
 
+    JMenuBar menuBar;
+    JMenu menu;
+//    JMenu othermenus;
 
     Database_checker() {
 
@@ -13,27 +16,41 @@ public class Database_checker extends JFrame {
         this.setTitle("Database checker");
         this.setLocationRelativeTo(null);
         this.setLayout(new FlowLayout());
+
+        menuBar = new JMenuBar();
+        menu = new JMenu("File");
+
+//        Now add menu to menu bar
+        
+
+
+        this.setJMenuBar(menuBar);
+
+
+//      display the frame
         this.setVisible(true);
 
 
-        mb = new JMenuBar();
-        x = new JMenu();
 
-        m1 = new JMenuItem("List Databases");
-        m2 = new JMenuItem("List Database Entries");
-        m3 = new JMenuItem("Delete Database/s");
-        m4 = new JMenuItem("Create New Database/s");
-        m5 = new JMenuItem("Close Program");
 
-        x.add(m1);
-        x.add(m2);
-        x.add(m3);
-        x.add(m4);
-        x.add(m5);
-        mb.add(x);
-
-        f.add(mb, BorderLayout.NORTH);
-        f.setJMenuBar(mb);
+//        mb = new JMenuBar();
+//        x = new JMenu();
+//
+//        m1 = new JMenuItem("List Databases");
+//        m2 = new JMenuItem("List Database Entries");
+//        m3 = new JMenuItem("Delete Database/s");
+//        m4 = new JMenuItem("Create New Database/s");
+//        m5 = new JMenuItem("Close Program");
+//
+//        x.add(m1);
+//        x.add(m2);
+//        x.add(m3);
+//        x.add(m4);
+//        x.add(m5);
+//        mb.add(x);
+//
+//        f.add(mb, BorderLayout.NORTH);
+//        f.setJMenuBar(mb);
 
     }
 
