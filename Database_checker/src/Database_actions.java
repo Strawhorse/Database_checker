c7a55050b90bd819001a5154ec4428b1a3173b1a
Sean B <strawhorse@gmail.com>
2024-01-14 11:52:41 +0000

    Might redo project, main issue seems to be with GUI and reading in info

--- Database_checker/src/Database_actions.java
+++ Database_checker/src/Database_actions.java
@@ -4,8 +4,8 @@ import java.sql.*;
 public class Database_actions {
 
 //    enter database methods here
-    public static void databaseChecker() throws SQLException, ClassNotFoundException {
-        Connection con = DB.getCon();
+    public static void databaseChecker(String user, String password) throws SQLException, ClassNotFoundException {
+        Connection con = DB.getCon(user, password);
         System.out.println("Database connection reached...");
 //        String query = "SHOW DATABASES";
 //        String query2 = "select * from profile;";
@@ -13,65 +13,17 @@ public class Database_actions {
 //        p.executeUpdate();
 //        Might be a problem with this statement
 
-
-
-
-
-//Works now, was problem with JAR file
-
-    }
-
-    public static void listDatabaseEntries() throws SQLException {
-        System.out.println("Entries for database include:");
-
-        Connection con = DB.getCon();
-
         Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM PROFILE;");
+        ResultSet rs = stmt.executeQuery("SHOW DATABASES;");
 
-        while (rs.next()){
-            int id = rs.getInt("id");
-            String name = rs.getString("name");
-            String domain = rs.getString("domain");
-            String address = rs.getString("address");
-            System.out.format("%s, %s, %s, %s, id", id, name, domain, address);
-        }
         rs.close();
         stmt.close();
         con.close();
+
+        System.out.println("Worked ...");
+
+
     }
 
-
-
-    public boolean updateEntry(String newName, String newDomain, String newAddress, int id) {
-//        make use of MySQL update command for final crud operation
-
-        boolean check = false;
-        try{
-            Connection con = DB.getCon();
-            String query = new StringBuilder().append("UPDATE profile set name=\'").append(newName).append("\', domain=\'").append(newDomain).append("\', address=\'").append(newAddress).append("\' where id=").append(id).toString();
-
-//            Run command to delete entry
-            PreparedStatement p = con.prepareStatement(query);
-            p.execute();
-            System.out.println("Successfully updated");
-
-            System.out.println("Updated profile:\n");
-
-            String checkQuery = "Select * from profile where id= "+id;
-            Statement statement = con.createStatement();
-            ResultSet rs = statement.executeQuery(checkQuery);
-
-            while(rs.next()) {
-                System.out.println("\nID: " + rs.getInt(1) + "\nName: " + rs.getString(2) + "\nDomain: " + rs.getString(3) + "\nAddress: " + rs.getString(4));
-            }
-            check = true;
-        }
-        catch (Exception e){
-            System.out.println(e);
-        }
-
-        return check;
-    }
 
 }
