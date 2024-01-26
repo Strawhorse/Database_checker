461aafa3794d63306f2d048838e42b736cfcdded
Sean B <strawhorse@gmail.com>
2024-01-26 16:01:49 +0000

    Going to rebuild application in JavaFx

--- Database_checker/src/Database_checker.java
+++ Database_checker/src/Database_checker.java
@@ -1,101 +1,14 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
-public class Database_checker extends JFrame implements ActionListener {
-
-//    create instances of necessary variables
-    JMenuBar menuBar;
-    JMenu menu;
-    JMenu menu2;
-    JMenuItem m1;
-    JMenuItem m2;
-    JMenuItem m3;
-    JMenuItem m4;
-    JMenuItem m5;
-
-
-//    JOptionPane for taking in database information
+public class Database_checker {
 
 
 
     Database_checker() {
 
-//        set out the JFrame first
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(800,800);
-        this.setTitle("Database checker");
-        this.setLocationRelativeTo(null);
-        this.setLayout(new FlowLayout());
+//  replace Swing with JavaFX
 
 
-
-//      display the frame
-        this.setVisible(true);
-
-
-    }
-
-
-
-    //    Implemented ActionListener so much create actionPerformed method for the menu items
-//    Use separate classes and methods to carry out actions
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-        if(e.getSource()==m1) {
-            System.out.println("Check Database list");
-
-            JTextField usernameField = new JTextField(10);
-            JTextField passwordField = new JTextField(10);
-
-            JPanel myPanel = new JPanel();
-            myPanel.add(new JLabel("Username:"));
-            myPanel.add(usernameField);
-            myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-            myPanel.add(new JLabel("Password:"));
-            myPanel.add(passwordField);
-
-            String username = usernameField.getText();
-            String pw = passwordField.getText();
-
-            int result = JOptionPane.showConfirmDialog(null, myPanel,
-                    "Please Enter SQL Username, and Password", JOptionPane.OK_CANCEL_OPTION);
-            if (result == JOptionPane.OK_OPTION) {
-                System.out.println("Username: " + usernameField.getText());
-                System.out.println("Password: " + passwordField.getText());
-            } else {
-                System.out.println("Nothing entered...");
-            }
-
-            try {
-                Database_actions.databaseChecker(username,pw);
-            } catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            } catch (ClassNotFoundException ex) {
-                throw new RuntimeException(ex);
-            }
-
-        }
-
-        if(e.getSource()==m2) {
-            System.out.println("Check database entries");
-        }
-
-        if(e.getSource()==m3) {
-            System.out.println("Delete database/s");
-        }
-
-        if(e.getSource()==m4) {
-            System.out.println("Create Database/s");
-        }
-
-        if(e.getSource()==m5) {
-            System.out.println("Close");
-            System.exit(0);
-        }
     }
 
 
@@ -105,52 +18,10 @@ public class Database_checker extends JFrame implements ActionListener {
 
 //    main method for instantiating the JFrame class
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Database_checker frame = new Database_checker();
+
+        System.out.println("Create instance here...");
     }
 
 
 
 }
-
-
-
-
-
-//Reusable code
-
-////        Then the menu bar and the menu/s
-//        menuBar = new JMenuBar();
-//                menu = new JMenu("Databases");
-//                menu2 = new JMenu("Other");
-//
-////        Now add menus to menu bar
-//                menuBar.add(menu);
-//                menuBar.add(menu2);
-//
-////        add items to the menus
-//                m1 = new JMenuItem("List Databases on computer");
-//                m2 = new JMenuItem("List Database Entries");
-//                m3 = new JMenuItem("Delete Database/s");
-//                m4 = new JMenuItem("Create New Database/s");
-//                m5 = new JMenuItem("Close Program");
-//
-//                menu.add(m1);
-//                menu.add(m2);
-//                menu.add(m3);
-//                menu.add(m4);
-//                menu2.add(m5);
-//
-//
-////        finally, set the menu bar to the frame
-//                this.setJMenuBar(menuBar);
-//
-//
-//
-//                //sub-menu items
-//                m1.addActionListener(this);
-//                m2.addActionListener(this);
-//                m3.addActionListener(this);
-//                m4.addActionListener(this);
-//                m5.addActionListener(this);
-//
-//
